ce62d4f48218e909acbd9530780ed425f28e539b
sagarkataria <sagarsingh0110@gmail.com>
2024-04-23 00:32:33 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,26 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const parent = React.createElement(
-                "div",
-                {id:"parent"},
-                React.createElement(
-                "div",
-                {id:"child"},
-                [
-                React.createElement(
-                "h1",
-                {},"I 'm h1 tag"),
-                React.createElement(
-                "h2",
-                {},"I 'm h1 tag sibling"),
-            ])
-        );
 
+// React.createElement => it's Object => HTMLElement(render)
+const heading = React.createElement("h1",{id:"heading"},"Hello React");
 
-const heading = React.createElement(
-    "h1", 
-    {id:"heading"}, 
-    "Hello World from React"
-);
+// JSX - HTML-like or xml syntax
+
+// jSX (Transpiled before it reaches the js)
+const jsxHeading = (
+                    <h1 id="heading1" className="heading" tabIndex="3">
+                       Namste React form jsx
+                    </h1>
+                   )
+
+// functional components
+
+const HeadingComponents = () => {
+    return <h1>This is functional components</h1>
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
